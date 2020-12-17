1a9482905daffc9415ac8bff9385f5602a9887d2
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-12-17 17:29:17 +0000

    rgb_values.py

---
+++ rgb_values.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# created by: Ryan Walsh
+# created on: December 2020
+# this program prints out all the valid RGB values
+
+
+def main():
+    # this program prints out all the valid RGB values
+    counter1 = 0
+    counter2 = 0
+    counter3 = 0
+
+    # process & output
+    for counter1 in range(255):
+        for counter2 in range(255):
+            for counter3 in range(255):
+                print("RGB({0},{1},{2}".format(counter1, counter2,
+                                               counter3))
+
+
+if __name__ == "__main__":
+    main()
