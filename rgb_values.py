457e92ee6b2378d1cfbe9a6ff647535fe729534f
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2020-12-17 12:31:40 -0500

    Update rgb_values.py

--- rgb_values.py
+++ rgb_values.py
@@ -12,9 +12,9 @@ def main():
     counter3 = 0
 
     # process & output
-    for counter1 in range(255):
-        for counter2 in range(255):
-            for counter3 in range(255):
+    for counter1 in range(256):
+        for counter2 in range(256):
+            for counter3 in range(256):
                 print("RGB({0},{1},{2}".format(counter1, counter2,
                                                counter3))
 
